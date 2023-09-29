820d8f09bc076ff8ccb82407cc945a25f1a66880
dmitr <dmitryoreskin@gmail.com>
2023-09-29 17:57:59 +0300

    Подготовка каталогов и создание root

---
+++ app/src/main/java/ru/practicum/android/diploma/core/root/RootActivity.kt
@@ -0,0 +1,15 @@
+package ru.practicum.android.diploma.core.root
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RootActivity : AppCompatActivity() {}
+   // private lateinit var binding: ActivityRootBinding
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityRootBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//        supportActionBar?.hide()
+//
+//}
