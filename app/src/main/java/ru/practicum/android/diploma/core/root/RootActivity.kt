81a20b2952c4f6a0a29c2a8859e22ea06d1cc372
Keiyakushi <nikulin-7@ya.ru>
2023-09-30 20:11:14 +0300

    Настройка навигации bottom_navigation,создал 3 фрагмента:поиск,избранное,команда,донастроил градл,убрал дубликат item_vacancy,создал nav_graph.xml,привязал его к root_activity.

--- app/src/main/java/ru/practicum/android/diploma/core/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/root/RootActivity.kt
@@ -2,14 +2,28 @@ package ru.practicum.android.diploma.core.root
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 //import org.koin.android.BuildConfig
 //import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRootBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+
+            }
+        }
 
         // Пример использования access token для HeadHunter API
 //        networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
---
+++ app/src/main/java/ru/practicum/android/diploma/team/TeamFragment.kt
@@ -0,0 +1,28 @@
+package ru.practicum.android.diploma.team
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FragmentFavoriteBinding
+import ru.practicum.android.diploma.databinding.FragmentTeamBinding
+
+class TeamFragment : Fragment() {
+    private lateinit var binding: FragmentTeamBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentTeamBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
