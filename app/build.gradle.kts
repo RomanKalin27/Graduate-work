b5587d0294dd41efadbd8f9b0183af8c2d51f530
dmitr <dmitryoreskin@gmail.com>
2023-09-30 21:10:12 +0300

    Мелкие изменения gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id("ru.practicum.android.diploma.plugins.developproperties")
 }
 kotlin {
-    jvmToolchain(8)
 }
 android {
     namespace = "ru.practicum.android.diploma"
@@ -30,11 +29,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures {
