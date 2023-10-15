91615684c70471b506a745382c64f7f9daff0156
Keiyakushi <nikulin-7@ya.ru>
2023-10-15 15:52:12 +0300

    Смена верстки item rv ,убрана кнопка, добавленно - при выборе региона возвращаться с результатом назад

--- app/src/main/java/ru/practicum/android/diploma/filters/presentation/rv/RegionAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/presentation/rv/RegionAdapter.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.filters.presentation.rv
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import ru.practicum.android.diploma.databinding.ItemCountryBinding
 import ru.practicum.android.diploma.databinding.ItemRegionBinding
 import ru.practicum.android.diploma.search.data.dto.response_models.Area
 
@@ -10,7 +11,7 @@ class RegionAdapter(private val clickListener: RegionClickListener) :
     RecyclerView.Adapter<RegionViewHolder>() {
     var regionList = ArrayList<Area>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = RegionViewHolder(
-        ItemRegionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        ItemCountryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
     )
 
 
@@ -19,7 +20,6 @@ class RegionAdapter(private val clickListener: RegionClickListener) :
         holder.bind(regionList[position])
         holder.itemView.setOnClickListener {
             clickListener.onTrackClick(regionList[position])
-            holder.radioButton.isChecked = !holder.radioButton.isChecked
         }
 
     }
