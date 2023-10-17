372b0e4387b8044eea6084dda6c9daa874962fd6
Gerasim <g-926@ya.ru>
2023-10-17 22:54:38 +0300

    Универсальный биндинг с зщитой от утечек

--- app/src/main/java/ru/practicum/android/diploma/team/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/team/TeamFragment.kt
@@ -1,23 +1,13 @@
 package ru.practicum.android.diploma.team
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.common.utils.BindingFragment
 import ru.practicum.android.diploma.databinding.FragmentTeamBinding
 
-class TeamFragment : Fragment() {
-    private lateinit var binding: FragmentTeamBinding
+class TeamFragment : BindingFragment<FragmentTeamBinding>() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        super.onCreateView(inflater, container, savedInstanceState)
-        binding = FragmentTeamBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun createBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentTeamBinding {
+        return FragmentTeamBinding.inflate(inflater, container, false)
     }
-
 }
